826a7bd7f44a2ce5a76f1042ab3271cfe34b464c
ios4212 <ios4212@caelum57-02.local>
2014-01-13 15:01:08 -0200

    Initial Commit

---
+++ Contatos/VHMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VHMAppDelegate.h
+//  Contatos
+//
+//  Created by ios4212 on 13/01/14.
+//  Copyright (c) 2014 caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VHMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Contatos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Contatos
+//
+//  Created by ios4212 on 13/01/14.
+//  Copyright (c) 2014 caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VHMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VHMAppDelegate class]));
+    }
+}
